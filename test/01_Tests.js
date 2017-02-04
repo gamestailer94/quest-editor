1e2b3649efc3a87ebc2fa0873012ab5008d88a39
Tim Platzke <tim@platzke.de>
2017-02-04 03:19:44 +0100

    Tests
    + new tests
    + test files
    + mocha options

--- test/01_Tests.js
+++ test/01_Tests.js
@@ -1,33 +1,40 @@
 /* eslint-env mocha */
-var Application = require('spectron').Application
+'use strict'
+let Application = require('spectron').Application
 var chai = require('chai')
 var chaiAsPromised = require('chai-as-promised')
 var path = require('path')
-var electronPath = require('electron')
+var fs = require('fs')
 
 chai.should()
 chai.use(chaiAsPromised)
 
-describe('application launch', function () {
+describe('QuestEditor', function () {
   this.timeout(300000)
-  beforeEach(function () {
-    this.App = new Application({
-      path: electronPath,
-      args: [path.join(__dirname, '../app')],
-      chromeDriverLogPath: 'test.log'
-    })
-    chaiAsPromised.transferPromiseness = this.App.transferPromiseness
-    return this.App.start()
+
+  this.slow(60000)
+
+  var App = new Application({
+    path: require('electron'),
+    args: [path.join(__dirname, '../app')]
   })
 
-  afterEach(function () {
-    if (this.App && this.App.isRunning()) {
-      return this.App.stop()
+  before(function () {
+    return App.start()
+  })
+
+  beforeEach(function () {
+    chaiAsPromised.transferPromiseness = App.transferPromiseness
+  })
+
+  after(function () {
+    if (App && App.isRunning()) {
+      return App.stop()
     }
   })
 
-  it('opens a window', function () {
-    return this.App.client.waitUntilWindowLoaded()
+  it('should start', function () {
+    return App.client.waitUntilWindowLoaded()
       .getWindowCount().should.eventually.equal(1)
       .browserWindow.isMinimized().should.eventually.be.false
       .browserWindow.isDevToolsOpened().should.eventually.be.false
@@ -36,29 +43,49 @@ describe('application launch', function () {
       .browserWindow.getBounds().should.eventually.have.property('height').and.be.above(0)
   })
 
-  it('should have nav elements', function () {
-    return this.App.client.waitUntilWindowLoaded()
-      .isExisting('#bottom-bar').should.eventually.be.true
-      .isVisible('#bottom-bar').should.eventually.be.true
-  })
+  describe('Main Window', function () {
+    this.slow(4000)
 
-  it('deletes localStorage', function () {
-    return this.App.client.localStorage('DELETE')
-  })
+    it('should have nav elements', function () {
+      return App.client.waitUntilWindowLoaded()
+        .isVisible('#bottom-bar').should.eventually.be.true
+    })
 
-  it('tests if no folder warning is visible', function () {
-    return this.App.client.waitUntilWindowLoaded()
-      .isExisting('#noFolder').should.eventually.be.true
-      .isVisible('#noFolder').should.eventually.be.true
-  })
+    it('should have no folder warning', function () {
+      return App.client.localStorage('DELETE').then(function () {
+        return App.client.refresh()
+          .waitForExist('#noFolder').should.eventually.be.true
+          .isVisible('#noFolder').should.eventually.be.true
+      })
+    })
 
-  it('sets localStorage', function () {
-    return this.App.client.localStorage('POST', {'key': 'projectFolder', 'value': 'foo'})
-  })
+    it('should have wrong folder warning', function () {
+      return App.client.localStorage('POST', {'key': 'projectFolder', 'value': 'foo'}).then(function () {
+        return App.client.refresh()
+          .waitForExist('#wrongFolder').should.eventually.be.true
+          .isVisible('#wrongFolder').should.eventually.be.true
+      })
+    })
 
-  it('test if wrong folder warning is visible', function () {
-    return this.App.client.waitUntilWindowLoaded()
-      .isExisting('#wrongFolder').should.eventually.be.true
-      .isVisible('#wrongFolder').should.eventually.be.true
+    it('should have Quests Overview with 1+ elements', function () {
+      let projectDir = path.join(__dirname, 'files')
+      let newPlugin = fs.readFileSync(path.join(__dirname, '../app/res/GS_QuestSystem.js'))
+      fs.writeFileSync(path.join(projectDir, '/js/plugins/GS_QuestSystem.js'), newPlugin)
+
+      return App.client.localStorage('POST', {'key': 'projectFolder', 'value': projectDir}).then(function () {
+        return App.client.refresh()
+          .waitForExist('#quests').should.eventually.be.true
+          .isVisible('#quests').should.eventually.be.true
+          .$$('#quests > .panel').should.eventually.have.length.above(1)
+      })
+    })
+
+    it('should have steps', function () {
+      return App.client.isExisting('#quests > .panel .bodySteps').should.eventually.be.true
+    })
+
+    it('should have rewards', function () {
+      return App.client.isExisting('#quests > .panel .bodyRewards').should.eventually.be.true
+    })
   })
 })
